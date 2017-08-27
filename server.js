1d9abed90cb3c73b401cc6b109ac515b588be376
Nigel Davis <nigeledavis@ufl.edu>
2017-08-27 11:28:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ var requestHandler = function(request, response) {
     response.end();
   }
   
+  //Else statement that writes "Bad gateway error" to the client if they did not issue a GET request to "listings"
   else {
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.write("Bad gateway error");
@@ -33,11 +34,12 @@ var requestHandler = function(request, response) {
 
 };
 
-
+//function that reads data from the listings.json file and saves that data in the variable listingData
 fs.readFile('listings.json', 'utf8', function(err, data) {
 if (err) throw err;
 listingData = data;
 
+//starting the server on port 8080
 http.createServer(requestHandler).listen(8080);
 console.log('Server started');
 });
